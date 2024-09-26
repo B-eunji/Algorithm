bdb97782ca5b20ce5e95567421d47818ff1af133
B-eunji <80443976+B-eunji@users.noreply.github.com>
2024-09-26 22:58:29 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,17 @@
+King, Queen, Look, Be, Night, Phone = map(int,input().split())
+King_A = 1
+Queen_A = 1
+Look_A = 2
+Be_A = 2
+Night_A = 2
+Phone_A = 8
+
+King_out = King_A - King
+Queen_out = Queen_A - Queen
+Look_out = Look_A - Look
+Be_out = Be_A - Be
+Night_out = Night_A - Night
+Phone_out = Phone_A - Phone
+
+
+print(King_out, Queen_out, Look_out, Be_out, Night_out,Phone_out) 
