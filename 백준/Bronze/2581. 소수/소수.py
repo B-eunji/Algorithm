e9f75a014ad30325113b0f2ec323451ed127d152
B-eunji <80443976+B-eunji@users.noreply.github.com>
2024-10-16 20:31:48 +0900

    [Bronze II] Title: 소수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,21 +1,21 @@
 M = int(input())
 N = int(input())
-prime_sum = 0
+prime_add = 0
 prime_list = []
 
-for i in range(M, N+1):
+for i in range(M,N+1):
     if i > 1:
         is_prime = True
-        for j in range(2,int(i ** 0.5) + 1):
-            if i % j == 0:
+        for j in range(2,int(i**0.5)+1):
+            if i % j ==0:
                 is_prime = False
                 break
         if is_prime:
+            prime_add += i
             prime_list.append(i)
-            prime_sum += i
-    
-if prime_list:
-    print(prime_sum)
-    print(prime_list[0])
-else:
+
+if not prime_list:
     print(-1)
+else:
+    print(prime_add)
+    print(prime_list[0])
