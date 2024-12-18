172f0a4a89a1156599cda3bc7d7cc0b07763852c
B-eunji <80443976+B-eunji@users.noreply.github.com>
2024-12-18 11:30:24 +0900

    [Silver IV] Title: 문자열 집합, Time: 596 ms, Memory: 37432 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,10 @@
+N,M = map(int,input().split())
+S = set(input().strip() for _ in range(N))
+
+count = 0
+for _ in range(M):
+    word = input().strip()
+    if word in S:
+        count += 1
+    
+print(count)
