d529aaa7cd0a86e9338cfd71036df0182d1dad55
B-eunji <80443976+B-eunji@users.noreply.github.com>
2025-03-07 14:28:49 +0900

    [Bronze IV] Title: 주사위 세개, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,13 @@
+A, B, C = map(int,input().split())
+
+if A==B==C :
+    coin = 10000 + (A * 1000)
+elif A==B or A==C:
+    coin = 1000 + (A*100)
+elif B==C:
+    coin = 1000 + (B*100)
+else:
+    coin = max(A,B,C)*100
+
+print(coin)
+    
