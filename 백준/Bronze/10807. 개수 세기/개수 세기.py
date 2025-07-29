bfe4701928e11ead51189299c74a09d1cb51e8e5
B-eunji <80443976+B-eunji@users.noreply.github.com>
2025-07-29 15:25:44 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -3,8 +3,7 @@ numbers = list(map(int,input().split()))
 v = int(input())
 
 count = 0
-
-for num in numbers:
-    if num == v:
-        count+=1
+for i in numbers:
+    if i == v:
+        count += 1
 print(count)
