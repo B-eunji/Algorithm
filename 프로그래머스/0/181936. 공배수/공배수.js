eaec2f637481cb445bc6d5fd1639016c194dc76f
B-eunji <80443976+B-eunji@users.noreply.github.com>
2025-09-03 16:13:38 +0900

    [level 0] Title: 공배수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.js
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -1,6 +1,6 @@
 function solution(number, n, m) {
     var answer = 0;
-    if (number % n == 0 && number % m == 0){
+    if(number % n == 0 && number % m == 0){
         answer = 1;
     }
     else{
