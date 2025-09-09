7486976b0a67b94bdd7c3652e3334ac81b55223a
B-eunji <80443976+B-eunji@users.noreply.github.com>
2025-09-09 16:46:22 +0900

    [level 0] Title: 코드 처리하기, Time: 8.68 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,19 @@
+function solution(code) {
+    let ret = "";
+    let mode = 0;
+
+    for (let idx = 0; idx < code.length; idx++) {
+        if (code[idx] === "1") {
+            // mode를 토글
+            mode = mode === 0 ? 1 : 0;
+        } else {
+            if (mode === 0 && idx % 2 === 0) {
+                ret += code[idx];
+            } else if (mode === 1 && idx % 2 === 1) {
+                ret += code[idx];
+            }
+        }
+    }
+
+    return ret.length === 0 ? "EMPTY" : ret;
+}
