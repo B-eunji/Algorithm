5abf0ffff5d939247f0a8748aba03419b503b576
B-eunji <80443976+B-eunji@users.noreply.github.com>
2025-09-23 16:19:35 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.91 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,16 @@
+function solution(n, control) {
+    var answer = 0;
+    for (let ch of control){
+        if(ch==="w"){
+            n+=1;
+        }else if(ch==="s"){
+            n-=1
+        }else if(ch==="d"){
+            n+=10;;
+        }else if(ch==="a"){
+            n-=10;
+        }
+    }
+    answer = n
+    return answer;
+}
