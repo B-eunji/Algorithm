42dd31c6b3dce7c8904f3c6ec2ed40899b3851a1
B-eunji <80443976+B-eunji@users.noreply.github.com>
2026-03-18 20:16:30 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,7 +1,8 @@
-A = int(input())
-year_1 = A % 4==0 and A % 100 != 0
-year_2 = A % 4==0 and A % 400 ==0
-if year_1 or year_2:
-    print("1")
+year = int(input())
+if(year % 4 == 0):
+    if(year % 100 != 0 or year % 400 == 0):
+        print(1)
+    else:
+        print(0)
 else:
-    print("0")
+    print(0)
