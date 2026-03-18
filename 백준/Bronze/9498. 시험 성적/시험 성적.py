f0e9fc870bbab6d7212dfc5f7df588c59bf974bb
B-eunji <80443976+B-eunji@users.noreply.github.com>
2026-03-18 20:13:04 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,11 +1,12 @@
-A = int(input())
-if A>=90:
-    print("A")
-elif A>=80:
-    print("B")
-elif A>=70:
-    print("C")
-elif A>=60:
-    print("D")
+score = int(input())
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
 else:
-    print("F")
+    print('F')
+    
