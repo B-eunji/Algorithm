d386856e931a0729011cfaeca8060a75e1539339
B-eunji <80443976+B-eunji@users.noreply.github.com>
2026-03-25 15:54:07 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,22 +1,23 @@
-words = input()
-total_time = 0
+minute = 0
 
-for char in words:
-    if char in 'ABC':
-        total_time += 3
-    elif char in 'DEF':
-        total_time += 4
-    elif char in 'GHI':
-        total_time += 5
-    elif char in 'JKL':
-        total_time += 6
-    elif char in 'MNO':
-        total_time +=7
-    elif char in 'PQRS':
-        total_time += 8
-    elif char in 'TUV':
-        total_time += 9
-    elif char in 'WXYZ':
-        total_time += 10
+text = input().strip()
+
+for ch in text:
+    if ch in ['A' , 'B' , 'C']:
+        minute += 3
+    elif ch in ['D' , 'E' , 'F']:
+        minute += 4
+    elif ch in ['G' , 'H' , 'I']:
+        minute += 5
+    elif ch in ['J' , 'K' , 'L']:
+        minute += 6
+    elif ch in ['M' , 'N' , 'O']:
+        minute += 7
+    elif ch in ['P' , 'Q' , 'R' , 'S']:
+        minute += 8
+    elif ch in ['T' , 'U' , 'V']:
+        minute += 9
+    elif ch in ['W' , 'X' , 'Y' , 'Z']:
+        minute += 10
         
-print(total_time)
+print(minute)
